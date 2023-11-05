403fafb82aeae144a2cd55fea1ec125d681b64e2
Noah <noahmichaels@gmail.com>
2023-11-05 14:45:56 -0500

    Create Basic Form
    Add 2 text boxes, submit button and Header

--- projecttracking0.1/src/App.js
+++ projecttracking0.1/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Input, Stack, Button } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header"> 
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,17 @@ function App() {
           >
             Learn React
           </a>
+        </header>*/}
+        <header>
+          Updates for Project 1
         </header>
+        <body>
+          <Stack spacing={3}>
+            <Input placeholder='Contact' width='auto'/>
+            <Input placeholder='Notes' width='auto'/>
+            <Button>Submit</Button>
+          </Stack>
+        </body>
       </div>
     </ChakraProvider>
   );
