707baed458bd66af87c8b91e1d717ac96594c00c
kevinharps <33207609+kevinharps@users.noreply.github.com>
2023-11-05 15:09:13 -0500

    Adding the First Table

--- projecttracking0.1/src/App.js
+++ projecttracking0.1/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { ChakraProvider, Input, Stack, Button } from '@chakra-ui/react'
+import { ChakraProvider, Input, Center, Stack, Button } from '@chakra-ui/react'
+import { Table, Thead, Tbody, Tr, Th, Td, TableContainer} from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
       <div className="App">
-        {/* <header className="App-header"> 
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -24,11 +25,38 @@ function App() {
           Updates for Project 1
         </header>
         <body>
+        <Center>
           <Stack spacing={3}>
             <Input placeholder='Contact' width='auto'/>
             <Input placeholder='Notes' width='auto'/>
             <Button>Submit</Button>
           </Stack>
+          </Center>
+          <TableContainer>
+            <Table variant='striped' colorScheme='gray'>
+              <Thead>
+                <Tr>
+                  <Th>Contact</Th>
+                  <Th>Notes</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>Contact</Td>
+                  <Td>Notes</Td>
+                </Tr>
+                <Tr>
+                  <Td>Contact</Td>
+                  <Td>Notes</Td>
+                </Tr>
+                <Tr>
+                  <Td>Contact</Td>
+                  <Td>Notes</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+
         </body>
       </div>
     </ChakraProvider>
